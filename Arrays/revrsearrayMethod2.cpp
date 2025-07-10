400557f1013b92f94cf1e2cb697fa2b6b8709ae3
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2025-07-10 19:56:55 +0530

    experimenting something

--- Arrays/revrsearrayMethod2.cpp
+++ Arrays/revrsearrayMethod2.cpp
@@ -1,10 +1,46 @@
 #include<iostream>
 using namespace std;
 
+
+// this one is the best method for reversing array using recursion
+void reversingArray(int i , int arr[], int n){
+
+    // starting with base condition
+
+    if(i>=n/2) return ;
+
+
+    // logic for reversing the array using reccursion 
+    swap(arr[i],arr[n-i-1]);
+
+    // recursion calling it 
+    reversingArray(i+1, arr, n);
+
+}
+
+
+// main function for this one 
 int main (){
 
-    int a = 5 , b =5 , c =5 , d=10;
+    cout <<"Enter the size of an array"<< endl;
 
-    a = b + c == d;
-    cout<< a << endl;
+    int n ;
+    cin >> n;
+
+    int arr[n];
+
+   cout << "Enter input in an array"<< endl;
+
+    for(int i =0 ;i<n;i++) cin >> arr[i];
+
+    reversingArray(0,arr,n);
+
+    cout<< " Reversed array is : "<< " "<< endl;
+
+    for(int i = 0 ; i < n ; i++) cout << arr[i] << " ";
+
+    return 0;
+
+
+
 }
